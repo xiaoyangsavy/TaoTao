d43548d58929c0cb198a1930e39aa1b677e30681
savy <xiaoyangsavy@163.com>
2017-07-23 09:48:01 +0800

    加入商品列表

--- taotao-manager/taotao-manager-service/src/main/java/com/taotao/service/impl/ItemServiceImpl.java
+++ taotao-manager/taotao-manager-service/src/main/java/com/taotao/service/impl/ItemServiceImpl.java
@@ -1,10 +1,16 @@
 package com.taotao.service.impl;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import com.github.pagehelper.PageHelper;
+import com.github.pagehelper.PageInfo;
+import com.taotao.common.pojo.EasyUIDataGridResult;
 import com.taotao.mapper.TbItemMapper;
 import com.taotao.pojo.TbItem;
+import com.taotao.pojo.TbItemExample;
 import com.taotao.service.ItemService;
 
 @Service
@@ -12,12 +18,35 @@ public class ItemServiceImpl implements ItemService {
 
 	@Autowired
 	private TbItemMapper itemMapper;
-	
+
 	@Override
 	public TbItem getItemById(long itemId) {
- TbItem item = itemMapper.selectByPrimaryKey(itemId);
-		
+		TbItem item = itemMapper.selectByPrimaryKey(itemId);
+
 		return item;
 	}
 
+	/**
+	 * 使用分页插件进行列表数据的查询
+	 */
+	@Override
+	public EasyUIDataGridResult getItemList(int page, int rows) {
+
+		// 分页处理
+		PageHelper.startPage(page, rows);
+		// 执行查询
+		TbItemExample example = new TbItemExample();
+		// 添加条件
+		// Criteria criteria = example.createCriteria();
+		// criteria.andIdEqualTo(123l);
+		List<TbItem> list = itemMapper.selectByExample(example);
+		// 取total
+		PageInfo<TbItem> pageInfo = new PageInfo<>(list);
+		long total = pageInfo.getTotal();
+
+		// 创建返回值对象
+		EasyUIDataGridResult result = new EasyUIDataGridResult(total, list);
+
+		return result;
+	}
 }
--- taotao-manager/taotao-manager-web/src/main/java/com/taotao/controller/ItemController.java
+++ taotao-manager/taotao-manager-web/src/main/java/com/taotao/controller/ItemController.java
@@ -4,8 +4,10 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 
+import com.taotao.common.pojo.EasyUIDataGridResult;
 import com.taotao.pojo.TbItem;
 import com.taotao.service.ItemService;
 
@@ -27,5 +29,21 @@ public class ItemController {
 	public TbItem getItemById(@PathVariable(value="itemId") Long itemId) {
 		TbItem item = itemService.getItemById(itemId);
 		return item;
+	}
+	
+	/**
+	 * 查询商品列表
+	 * Integer 防止传入为空时报错
+	 * @ResponseBody 返回json数据
+	 */
+	@RequestMapping("/list")
+	@ResponseBody
+	public EasyUIDataGridResult getItemList(@RequestParam(defaultValue="1") Integer page, 
+			@RequestParam(defaultValue="30") Integer rows) {
+		//调用service查询商品列表
+		EasyUIDataGridResult result = itemService.getItemList(page, rows);
+		//返回结果
+		return result;
+		
 	}
 }
